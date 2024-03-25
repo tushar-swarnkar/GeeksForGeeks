f88d01d36c7d9de561fc057ae39c1a6afa15b247
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-25 11:54:18 +0530

    day 25, ques 25

---
+++ DAY25_nBitBinaryNum.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DAY25_nBitBinaryNum {
+    public static void main(String[] args) {
+        
+    }
+    void solve(int n, ArrayList<String> arr,String temp,int countZ,int countO,boolean f){
+        if(n==0){
+            if(f){
+                arr.add(temp);
+            }
+            return;
+        }
+        if(countO>countZ)
+        solve(n-1,arr,temp+"0",countZ+1,countO,true);
+        if(countO>=countZ)
+        solve(n-1,arr,temp+"1",countZ,countO+1,true);
+    }
+    ArrayList<String> NBitBinary(int N) {
+        // code here
+        ArrayList<String> ans=new ArrayList<>();
+        solve(N,ans,"",0,0,true);
+        Collections.sort(ans,Collections.reverseOrder());
+        return ans;
+    }
+}
