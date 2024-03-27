bb3fd64c2225e257c95dc19ab44f657f2ebd7072
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-27 09:32:22 +0530

    day 27, ques 27

---
+++ DAY27_shortestSafePath.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DAY27_shortestSafePath {
+    public static void main(String[] args) {
+        int[][] mat = {{1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 0, 1},{1, 1, 1, 1, 0}};
+
+        System.out.println(findShortestPath(mat));
+    }
+    static int findShortestPath(int[][] mat) {
+        // code here
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] ans = new int[n][m];
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0, 1, 0, -1};
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(ans[i], 1);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    for (int k = 0; k < 4; k++) {
+                        int nr = delrow[k] + i;
+                        int nc = delcol[k] + j;
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                            ans[nr][nc] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (ans[i][0] == 1) {
+                queue.offer(new int[]{i, 0, 1});
+            }
+        }
+        int[][] vis = new int[n][m];
+        while (!queue.isEmpty()) {
+            int[] a = queue.poll();
+            int row = a[0];
+            int col = a[1];
+            int count = a[2];
+            vis[row][col] = 1;
+            if (col == m - 1) {
+                return count;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    int nr = delrow[i] + row;
+                    int nc = delcol[i] + col;
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && vis[nr][nc] == 0 && ans[nr][nc] == 1) {
+                        vis[nr][nc] = 1;
+                        queue.offer(new int[]{nr, nc, count + 1});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
