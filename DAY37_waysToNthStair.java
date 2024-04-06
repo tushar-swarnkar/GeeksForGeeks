16d25fdc4126ce534a29e08c5fb9435cf37e07d3
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-04-06 20:29:31 +0530

    day 37, ques 37

---
+++ DAY37_waysToNthStair.java
@@ -0,0 +1,18 @@
+public class DAY37_waysToNthStair {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countWays(j));
+    }
+    static Long countWays(int n) {
+        // your code here
+        long count = 0;
+        int copy = n;
+        
+        while (copy != 0 && copy !=1 ) {
+            copy = copy - 2;
+            count++;
+        }
+        
+        return count + 1;
+    }
+}
