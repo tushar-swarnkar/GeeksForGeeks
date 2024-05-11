adefb8fca187bb292f41891ff63333cee8f767c0
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-05-11 13:25:46 +0530

    day 73, ques 73

---
+++ DAY73_jugglerSequence.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAY73_jugglerSequence {
+    public static void main(String[] args) {
+        
+    }
+    static List<Long> jugglerSequence(long n) {
+        // code here
+        List<Long> arr= new ArrayList<Long>();
+        arr.add(n);
+        
+        while(n>1){
+            if(n%2==0){
+                n =(long) Math.pow(n,(float)1/2);
+                arr.add(n);
+            }
+            else{
+                n= (long)Math.pow(n,(float)3/2);
+                arr.add(n);
+            }
+        }
+        return arr;
+    }
+}
