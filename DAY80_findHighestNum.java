134be6069a191d04eaaf74d7566435b52e7cc823
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-05-18 21:17:49 +0530

    day 80, ques 80

---
+++ DAY80_findHighestNum.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class DAY80_findHighestNum {
+    public static void main(String[] args) {
+
+    }
+
+    static int findPeakElement(List<Integer> a) {
+        int left = 0;
+        int right = a.size() - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (a.get(mid) > a.get(mid + 1)) {
+                right = mid;
+            } else {
+
+                left = mid + 1;
+            }
+        }
+        return a.get(left);
+    }
+}
