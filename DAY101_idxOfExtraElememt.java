6101473c6f0a7efc592aa546d6b6052821c6c59a
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-08 15:50:08 +0530

    day 101, ques 101

---
+++ DAY101_idxOfExtraElememt.java
@@ -0,0 +1,11 @@
+public class DAY101_idxOfExtraElememt {
+    static int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        for(int i=0;i<n-1;i++){
+            if(arr1[i]!=arr2[i]){
+                return i;
+            }
+        }
+        return n-1;
+    }
+}
