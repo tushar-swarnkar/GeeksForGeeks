26ae5d107bc5b5afe0964b6837097bfeb318dc4b
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-11 18:40:15 +0530

    day 104, ques 104

---
+++ DAY104_maxTipCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class DAY104_maxTipCalculator {
+    public static long maxTip(int n, int x, int y, int[] arr, int[] brr) {
+        class Order {
+            int tipA;
+            int tipB;
+            int diff;
+
+            public Order(int tipA, int tipB) {
+                this.tipA = tipA;
+                this.tipB = tipB;
+                this.diff = Math.abs(tipA - tipB);
+            }
+        }
+
+        Order[] orders = new Order[n];
+
+        // Create orders with the respective tips
+        for (int i = 0; i < n; i++) {
+            orders[i] = new Order(arr[i], brr[i]);
+        }
+
+        // Sort orders based on the absolute difference of tips in descending order
+        Arrays.sort(orders, new Comparator<Order>() {
+            public int compare(Order o1, Order o2) {
+                return o2.diff - o1.diff;
+            }
+        });
+
+        long totalTip = 0;
+        int countA = 0, countB = 0;
+
+        // Assign orders to maximize tips
+        for (Order order : orders) {
+            if ((order.tipA >= order.tipB && countA < x) || countB == y) {
+                totalTip += order.tipA;
+                countA++;
+            } else {
+                totalTip += order.tipB;
+                countB++;
+            }
+        }
+
+        return totalTip;
+    }
+}
