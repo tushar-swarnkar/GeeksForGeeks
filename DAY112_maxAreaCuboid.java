1f1ba9e07bcdf15b1e1c5d0d258a079f01e48f06
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-19 18:40:37 +0530

    day 112, ques 112

---
+++ DAY112_maxAreaCuboid.java
@@ -0,0 +1,8 @@
+public class DAY112_maxAreaCuboid {
+    double maxVolume(double perimeter, double area) {
+        double len = (double) (perimeter - Math.sqrt(perimeter * perimeter - 24 * area)) / 12;
+        double height = (perimeter / 4) - (2 * (len));
+        double res = len * len * height;
+        return res;
+    }
+}
