3b96b529368d209477a4b10d2b4f8c85abe60d39
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-22 23:09:29 +0530

    day 115, ques 115

---
+++ DAY115_extractNumFromStr.java
@@ -0,0 +1,16 @@
+public class DAY115_extractNumFromStr {
+    long ExtractNumber(String sentence) {
+        // code here
+        String[] arr = sentence.split("\\s+");
+        long largeNum = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isDigit(arr[i].charAt(0)) && !arr[i].contains("9")) {
+                long currNum = Long.parseLong(arr[i]);
+                if (largeNum < currNum) {
+                    largeNum = currNum;
+                }
+            }
+        }
+        return largeNum;
+    }
+}
