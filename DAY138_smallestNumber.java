0a323895f9b72bc671c291e58b6625625794316c
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-07-15 16:54:08 +0530

    day 138, ques 138

---
+++ DAY138_smallestNumber.java
@@ -0,0 +1,28 @@
+public class DAY138_smallestNumber {
+    public String smallestNumber(int s, int d) {
+        if (s > 9 * d)
+            return "-1";
+
+        int[] digits = new int[d];
+
+        s--; // Reserve 1 for the most significant digit if necessary
+        for (int i = d - 1; i > 0; i--) {
+            if (s > 9) {
+                digits[i] = 9;
+                s -= 9;
+            } else {
+                digits[i] = s;
+                s = 0;
+            }
+        }
+
+        digits[0] = s + 1; // Adjust the most significant digit
+
+        StringBuilder result = new StringBuilder();
+        for (int digit : digits) {
+            result.append(digit);
+        }
+
+        return result.toString();
+    }
+}
