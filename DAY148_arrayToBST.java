ef81f0e4218aa675695917811f96fdd243aa33a1
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-07-25 17:22:21 +0530

    day 148, ques 148

---
+++ DAY148_arrayToBST.java
@@ -0,0 +1,16 @@
+public class DAY148_arrayToBST {
+    public Node sortedArrayToBST(int[] nums) {
+        return solve(0, nums.length - 1, nums);
+    }
+
+    public Node solve(int l, int r, int[] nums) {
+        if (l > r)
+            return null;
+        int mid = l + (r - l) / 2;
+        Node root = new Node(nums[mid]);
+        root.left = solve(l, mid - 1, nums);
+        root.right = solve(mid + 1, r, nums);
+
+        return root;
+    }
+}
