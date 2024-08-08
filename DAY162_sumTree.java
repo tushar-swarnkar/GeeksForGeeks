f1353b79d264a98cc536ab997393c71938354bdc
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-08-08 23:21:06 +0530

    day 162, ques 162

---
+++ DAY162_sumTree.java
@@ -0,0 +1,30 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class DAY162_sumTree {
+    boolean isSumTree(Node root) {
+        // Your code here
+        if (root == null || (root.left == null && root.right == null))
+            return true;
+        int ls = findSum(root.left);
+        int rs = findSum(root.right);
+        if (ls + rs == root.data) {
+            if (isSumTree(root.left) && isSumTree(root.right))
+                return true;
+        }
+        return false;
+    }
+
+    int findSum(Node root) {
+        if (root == null)
+            return 0;
+        return root.data + findSum(root.left) + findSum(root.right);
+    }
+}
