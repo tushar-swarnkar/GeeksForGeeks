21761a50087e9cbc3d6c7886f905c1f5c9666606
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-09-11 03:40:33 +0530

    Day 195, ques 195

---
+++ DAY195_costOfRopes.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+public class DAY195_costOfRopes {
+    public long minCost(long[] arr) {
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+
+        for (long length : arr) {
+            minHeap.add(length);
+        }
+
+        long totalCost = 0;
+
+        while (minHeap.size() > 1) {
+            long first = minHeap.poll();
+            long second = minHeap.poll();
+
+            long cost = first + second;
+            totalCost += cost;
+
+            minHeap.add(cost);
+        }
+
+        return totalCost;
+    }
+}
