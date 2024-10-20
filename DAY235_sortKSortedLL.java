7b81bd20d3a980da3b422ec575fe6ce1e4448f0d
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-10-20 10:21:09 +0530

    day 235, ques 235

---
+++ DAY235_sortKSortedLL.java
@@ -0,0 +1,53 @@
+import java.util.PriorityQueue;
+
+public class DAY235_sortKSortedLL {
+
+    public DLLNode sortAKSortedDLL(DLLNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        PriorityQueue<DLLNode> minHeap = new PriorityQueue<>((a, b) -> a.data - b.data);
+        DLLNode newHead = null, last = null;
+
+        // Add first k+1 elements to the min heap
+        for (int i = 0; i <= k && head != null; i++) {
+            minHeap.add(head);
+            head = head.next;
+        }
+
+        // Process the remaining elements
+        while (!minHeap.isEmpty()) {
+            if (newHead == null) {
+                newHead = minHeap.poll();
+                newHead.prev = null;
+                last = newHead;
+            } else {
+                last.next = minHeap.poll();
+                last.next.prev = last;
+                last = last.next;
+            }
+
+            if (head != null) {
+                minHeap.add(head);
+                head = head.next;
+            }
+        }
+        last.next = null;
+
+        return newHead;
+    }
+
+}
+
+class DLLNode {
+    int data;
+    DLLNode next;
+    DLLNode prev;
+
+    DLLNode(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
