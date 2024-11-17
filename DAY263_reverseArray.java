1fe3241c842911b982575310e3b525eecabfe16f
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-11-17 17:43:49 +0530

    day 263, ques 263

---
+++ DAY263_reverseArray.java
@@ -0,0 +1,10 @@
+public class DAY263_reverseArray {
+    public void reverseArray(int arr[]) {
+        int len = arr.length;
+        for (int i = 0; i < len / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[len - i - 1];
+            arr[len - i - 1] = temp;
+        }
+    }
+}
