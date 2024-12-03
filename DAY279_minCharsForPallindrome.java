5039197eea0044c4b0e1b504c6f454819d73cd87
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-12-04 01:15:11 +0530

    day 279, ques 279g

---
+++ DAY279_minCharsForPallindrome.java
@@ -0,0 +1,36 @@
+public class DAY279_minCharsForPallindrome {
+    public static int[] computeLPS(String s) {
+        int n = s.length();
+
+        int pre = 0;
+        int suff = 1;
+
+        int[] lps = new int[n];
+
+        while (suff < n) {
+            if (s.charAt(pre) == s.charAt(suff)) {
+                pre++;
+                lps[suff] = pre;
+                suff++;
+            } else {
+                if (pre == 0) {
+                    lps[suff] = 0;
+                    suff++;
+                } else {
+                    pre = lps[pre - 1];
+                }
+            }
+        }
+
+        return lps;
+    }
+
+    public static int minChar(String s) {
+        String reverse = new StringBuilder(s).reverse().toString();
+        String concat = s + "#" + reverse;
+
+        int[] lps = computeLPS(concat);
+
+        return s.length() - lps[lps.length - 1];
+    }
+}
