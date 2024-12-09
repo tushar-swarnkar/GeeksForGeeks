4515d745be45393e4f4b4736fcc9ca32128642c1
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-12-10 01:48:56 +0530

    day 285, ques 285

---
+++ DAY285_insertInterval.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class DAY285_insertInterval {
+
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> al = new ArrayList<>();
+        boolean added = false;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] < newInterval[0] && intervals[i][1] < newInterval[0]) {
+                al.add(intervals[i]);
+            } else if (intervals[i][0] > newInterval[1] && intervals[i][1] > newInterval[1]) {
+                if (added == false) {
+                    al.add(newInterval);
+                    added = true;
+                }
+                al.add(intervals[i]);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+        if (added == false) {
+            al.add(newInterval);
+        }
+        return al;
+    }
+
+}
