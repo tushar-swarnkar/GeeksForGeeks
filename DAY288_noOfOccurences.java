981f2b26400f44634367e8fdb34d2ccf3234b252
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-12-12 23:35:58 +0530

    day 288, ques 288g

---
+++ DAY288_noOfOccurences.java
@@ -0,0 +1,11 @@
+public class DAY288_noOfOccurences {
+    int countFreq(int[] arr, int target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
